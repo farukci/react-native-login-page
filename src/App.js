41742844f7b0efb2e6bea681e3ae94c15992f8e8
farukci <ferruhcihan20@gmail.com>
2020-10-05 23:30:11 +0300

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
+
 import React from 'react';
-// import MyComponent from './components/MyComponent';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
+  StatusBar, TextInput
 } from 'react-native';
 
 import ShoppingCart from './components/ShoppingCart';
 
 
 const App = () => {
-
-  const isAdmin = true;
-  function myFunction(userName) {
-    return <Text>{userName}</Text>
-
-  }
-
   return (
     <View style={styles.body}>
       <ShoppingCart/>
-      <View style={styles.container} marginTop={100} >
-        <Text style={styles.text}>
-          CLARUSWAY
-    </Text>
-      </View>
-      <View style={styles.container} padding={50} margin={20}>
-        <Text style={styles.text}>
-          FULLSTACK EKIBI
-    </Text>
-      </View>
-      {/* <MyComponent /> */}
-      {/* <Codehackers /> */}
+      <TextInput
+        style={styles.form}
+        placeholder="E-posta giriniz..."
+        defaultValue={""}
+      />
+      <TextInput
+        style={styles.form}
+        placeholder="Şifre giriniz..."
+        defaultValue={""}
+      />
+
+
+      
+      
     </View>
   );
 
@@ -43,28 +38,15 @@ const styles = StyleSheet.create({
   body: {
     backgroundColor: "green",
     display: "flex",
-    justifyContent: "space-evenly",
+    // justifyContent: "space-evenly",
     height: 800,
-  },
-  
-  
-  container: {
-    backgroundColor: 'orange',
+  },  
+  form:{
     margin: 20,
-    padding: 10,
-    borderRadius: 10,
+    borderColor: "black",
+    height: 40, 
+    backgroundColor: "white"
   },
-  text: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 30,
-    textAlign: 'center',
-  },
-  hello: {
-    margin: 10,
-    textAlign: 'center',
-    fontSize: 30
-  }
 
 });
 
